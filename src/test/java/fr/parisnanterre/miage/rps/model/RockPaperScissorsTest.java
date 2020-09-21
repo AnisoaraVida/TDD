af2274664467c34737096a2144adb706db5c17a6
anisoaravida <Olaf120697>
2020-09-21 18:30:17 +0200

    test data provider

--- src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
@@ -1,12 +1,8 @@
 package fr.parisnanterre.miage.rps.model;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import static fr.parisnanterre.miage.rps.model.Play.ROCK;
-import static fr.parisnanterre.miage.rps.model.Play.SCISSORS;
+import static fr.parisnanterre.miage.rps.model.Play.*;
 import static fr.parisnanterre.miage.rps.model.Result.*;
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -24,21 +20,59 @@ public class RockPaperScissorsTest {
     public void tearDown() {
         rps = null;
     }
+
     @Parameters({"papier", "pierre"})
     @Test
-    public void testPlay(String p1, String p2) {
+    public void testWin(String p1, String p2) {
         assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), WIN);
     }
 
     @Parameters({"pierre", "pierre"})
     @Test
-    public void testPlay1(String p1, String p2) {
-        assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), LOST);
+    public void testLost(String p1, String p2) {
+        assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), TIE);
     }
 
     @Parameters({"pierre", "papier"})
     @Test
-    public void testPlay2(String p1, String p2) {
-        assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), WIN);
+    public void testTie(String p1, String p2) {
+        assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), LOST);
     }
+
+    @DataProvider(name = "winData")
+    public Object[][] createWinData() {
+        return new Object[][]{{ PAPER, ROCK},
+                {PAPER, PAPER},
+                {ROCK, SCISSORS}};
+    }
+
+    @DataProvider(name = "lostData")
+    public Object[][] createLostData() {
+        return new Object[][]{{ROCK, PAPER},
+                {PAPER, SCISSORS},
+                {SCISSORS, ROCK}};
+    }
+
+    @DataProvider(name = "tieData")
+    public Object[][] createTieData() {
+        return new Object[][]{{PAPER, ROCK},
+                {ROCK, PAPER},
+                {SCISSORS, SCISSORS}};
+    }
+
+    @Test(dataProvider = "winData")
+    public void testWinData(Play p1, Play p2){
+        assertEquals(rps.play(p1, p2), WIN);
+    }
+
+    @Test(dataProvider = "lostData")
+    public void testLostData(Play p1, Play p2){
+        assertEquals(rps.play(p1, p2), LOST);
+    }
+
+    @Test(dataProvider = "tieData")
+    public void testTieData(Play p1, Play p2){
+        assertEquals(rps.play(p1, p2), TIE);
+    }
+
 }
