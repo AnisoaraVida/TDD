7caec6a6a763cc651fd690fcd1bde9e095af50a0
anisoaravida <Olaf120697>
2020-09-21 17:58:58 +0200

    test 1 parameters

--- src/main/java/fr/parisnanterre/miage/rps/model/RockPaperScissors.java
+++ src/main/java/fr/parisnanterre/miage/rps/model/RockPaperScissors.java
@@ -2,7 +2,7 @@ package fr.parisnanterre.miage.rps.model;
 
 public class RockPaperScissors {
     Result play(Play p1, Play p2){
-        return Result.LOST;
+        return Result.WIN;
     }
 
 }
--- src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
@@ -2,6 +2,7 @@ package fr.parisnanterre.miage.rps.model;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static fr.parisnanterre.miage.rps.model.Play.ROCK;
@@ -24,9 +25,9 @@ public class RockPaperScissorsTest {
     public void tearDown() {
         rps = null;
     }
-
+    @Parameters({"papier", "pierre"})
     @Test
-    public void testPlay() {
-        assertEquals(rps.play(SCISSORS, ROCK), LOST);
+    public void testPlay(String p1, String p2) {
+        assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), WIN);
     }
 }
